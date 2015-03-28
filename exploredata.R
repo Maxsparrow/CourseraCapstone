0ca684cd502e4d557e190b43b63c85b1d139547d
Maxsparrow <Maxsparrow@gmail.com>
2015-03-27 20:17:45 -0500

    Swapped out old explore data file

--- exploredata.R
+++ exploredata.R
@@ -1,32 +1,49 @@
-dirpath = 'final/en_US/'
-readtext <- function(filename) {
-	print(paste0('Reading in file: ',filename))
-	con<-file(paste0(dirpath,filename),open="r")
-	data<-readLines(con)
-	print(paste0('Number of lines in file: ',length(data)))
-	return(data)
-	close(con)
-}
-longestline <- function(data) {
-	longestline <- 0
-	curline <- 0
-	for (line in data) {
-		curline<-nchar(line)
-		if(curline>longestline) longestline<-curline
-	}
-	print(paste0('Length of longest line in file: ',longestline))
-}
-stringsearch <- function(data,searchstring,verbose=T) {
-	filtereddata <- data[grep(searchstring,data)]
-	numoflines<-length(filtereddata)
-	if(verbose) print(filtereddata)
-	print(paste0('number of matching lines: ',numoflines))
-	return(numoflines)
-}
-twitter <- readtext('en_US.twitter.txt')
-news <- readtext('en_US.news.txt')
-blogs <- readtext('en_US.blogs.txt')
+dirpath<-"Data Scientist Specialization/Capstone/final/en_US/"
 
-print(stringsearch(twitter,'love',verbose=F)/stringsearch(twitter,'hate',verbose=F))
-print(stringsearch(twitter,'biostats'))
-print(stringsearch(twitter,'A computer once beat me at chess, but it was no match for me at kickboxing'))
+readfile<-function(filetype) {
+    curfile<-file(paste0(dirpath,"en_US.",filetype,".txt"))
+    output<-readLines(curfile)
+    close(curfile)
+    return(output)
+}
+
+percofdocs<-function(text,file) {
+    count<-0
+    for(line in file) {
+        if(grepl(text,line)) {
+            print(line)
+            count<-count+1
+        }
+    }
+    print(paste0("Percentage of docs with this string: ",round(count/length(file)*100,2),"%"))
+}
+
+if(!exists("twitter")) twitter<-readfile("twitter")
+if(!exists("news")) news<-readfile("news")
+if(!exists("blogs")) blogs<-readfile("blogs")
+
+tokenizesample<-function(file,size) {
+    set<-sample(file,size)
+    wordlist<-vector()
+    for(item in set) {
+        cleaneditem<-gsub("[[:punct:]]"," ",item)
+        words<-strsplit(cleaneditem," ")[[1]]
+        for(word in words) {
+            if (word != "") wordlist<-c(wordlist,word)
+        }
+    }
+    return(wordlist)
+}
+
+analyzesample<-function(sample) {
+    wordlist<-unique(sample)
+    wordlist<-wordlist[sapply(wordlist,function(x) nchar(x)>2)]
+    wordframe<-data.frame(word=wordlist,count=0)
+    for(word in wordlist) {
+        wordcount<-sum(grepl(word,sample))
+        wordframe[wordframe$word==word,"count"]<-wordcount
+    }
+    wordframe<-wordframe[order(-wordframe$count),]
+    wordframe<-wordframe[wordframe$count>1,]
+    return(head(wordframe,100))
+}
