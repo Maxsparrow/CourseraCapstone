cff463845a5a17ed2780f9e6cfa135d9890459ab
Maxsparrow <Maxsparrow@gmail.com>
2015-03-28 20:44:53 -0500

    Improved exploration functions

--- exploredata.R
+++ exploredata.R
@@ -7,40 +7,74 @@ readfile<-function(filetype) {
     return(output)
 }
 
-percofdocs<-function(text,file) {
-    count<-0
-    for(line in file) {
-        if(grepl(text,line)) {
-            print(line)
-            count<-count+1
-        }
-    }
-    print(paste0("Percentage of docs with this string: ",round(count/length(file)*100,2),"%"))
-}
-
 if(!exists("twitter")) twitter<-readfile("twitter")
 if(!exists("news")) news<-readfile("news")
 if(!exists("blogs")) blogs<-readfile("blogs")
 
-tokenizesample<-function(file,size) {
-    set<-sample(file,size)
-    wordlist<-vector()
-    for(item in set) {
-        cleaneditem<-gsub("[[:punct:]]"," ",item)
-        words<-strsplit(cleaneditem," ")[[1]]
-        for(word in words) {
-            if (word != "") wordlist<-c(wordlist,word)
+summarizefile<-function(file) {
+    print(paste0('Number of documents in file: ',length(file)))
+    count<-0
+    sample<-sample(file,trunc(length(file)/10))
+    for(line in sample) {count<-count+length(strsplit(line," ")[[1]])}
+    print(paste0('Estimated number of words: ',count*10))
+}
+
+percofdocs<-function(searchtext,file) {
+    count<-0
+    sample<-sample(file,trunc(length(file)/10))
+    for(line in sample) {
+        if(grepl(searchtext,line)) {
+            count<-count+1
         }
     }
-    return(wordlist)
+    print(paste0("Estimated percentage of docs with this string: ",round(count*10/length(file)*100,2),"%"))
+}
+
+getsample<-function(file,size) {    
+    sample<-sample(file,size)
+    return(sample)
+}
+
+scrubsample<-function(sample) {
+    set<-vector()
+    for(item in sample) {
+        item<-gsub("<3"," heartsymbolplaceholder ",item)
+        item<-gsub("[-?:.\"<>!()_~`$%&*+,/;=@^]"," ",item)
+        item<-gsub("heartsymbolplaceholder","<3",item)
+        item<-gsub("[[:digit:]]+"," ::number:: ",item)
+        set<-append(set,item)
+    }
+    return(set)
+}
+
+tokenizesample<-function(sample,ngramsize) {
+    ngrams<-list()
+    item<-sample[1]
+    for(item in sample) {
+        words<-strsplit(item," ")[[1]]
+        counter<-1
+        while ((counter+ngramsize-1)<length(words)) {
+            set<-words[counter:(counter+ngramsize-1)]
+            counter<-counter+1
+            ngrams<-append(ngrams,list(set))
+        }
+    }
+    
+    return(ngrams)
 }
 
 analyzesample<-function(sample) {
-    wordlist<-unique(sample)
+    wordlist<-vector()
+    for(line in sample) {
+        for(word in line) {
+            wordlist<-append(wordlist,word)
+        }
+    }
+    wordlist<-unique(wordlist)
     wordlist<-wordlist[sapply(wordlist,function(x) nchar(x)>2)]
     wordframe<-data.frame(word=wordlist,count=0)
     for(word in wordlist) {
-        wordcount<-sum(grepl(word,sample))
+        wordcount<-sum(grepl(word,sample,fixed=T))
         wordframe[wordframe$word==word,"count"]<-wordcount
     }
     wordframe<-wordframe[order(-wordframe$count),]
